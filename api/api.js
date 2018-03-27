a759866e6350df079dc9cf8f975f698dec7c3f84
lijinyuan <lijinyuan@tospur.com>
2018-03-28 00:50:11 +0800

    榜单页面

--- api/api.js
+++ api/api.js
@@ -1,7 +1,9 @@
 module.exports = {
 	douban:{
 		index:'v2/movie/coming_soon',
-		new_movies:'/v2/movie/new_movies',
-		edit:'/v2/movie/subject'
+		new_movies:'v2/movie/new_movies',
+		edit:'v2/movie/subject',
+    in_theaters: 'v2/movie/in_theaters',
+    top250: 'v2/movie/top250'
 	}
 }
--- pages/board/board.js
+++ pages/board/board.js
@@ -1,18 +1,60 @@
 const app = getApp();
 Page({
   data: {
-    newmovies:[]
+    newmovies:[],
+    theaters:[],
+    boards:[],
   },
   handleClick(event) {
     app.navigateTo('../edit/edit',{
       id:event.currentTarget.dataset.item.id
     })
   },
+  getNewMovies(callback) { 
+    const restApi_new_movies = app.Api.douban.new_movies;
+    app.Rest('get', restApi_new_movies).then(res => {
+      if (callback) callback(res);
+    })
+  },
+  getNewTheaters(callback) { 
+    const restApi_in_theaters = app.Api.douban.in_theaters;
+    app.Rest('get', restApi_in_theaters, {
+      'start': 0,
+      'count': 8
+    }).then(res => {
+      if (callback) callback(res);
+    })
+  },
+  getNewTop250(callback) { 
+    const restApi_top250 = app.Api.douban.top250;
+    app.Rest('get', restApi_top250, {
+      'start': 0,
+      'count': 8
+    }).then(res => {
+      if (callback) callback(res);
+    })
+  },
   onLoad: function () {
-    const restApi = app.Api.douban.new_movies;
-    app.Rest('get',restApi).then(res=>{
+    wx.showLoading({ title: '拼命加载中...' });
+    var boardsLoad = [];
+    this.getNewMovies(res=>{
+      boardsLoad.push(res.data);
       this.setData({
-        newmovies: res.data.subjects
+        newmovies: res.data.subjects,
+        boards: boardsLoad
+      })
+      this.getNewTheaters(res => {
+        boardsLoad.push(res.data);
+        this.setData({
+          boards: boardsLoad
+        })
+        this.getNewTop250((res)=>{
+          boardsLoad.push(res.data);
+          this.setData({
+            boards: boardsLoad
+          })
+          wx.hideLoading();
+        })
       })
     })
   }
